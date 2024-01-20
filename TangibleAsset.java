a3a7a825d6a26733908022005a6226c4a1cd82c1
ususas0514 <157114596+ususas0514@users.noreply.github.com>
2024-01-20 20:43:39 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,15 @@
+public abstract class TangibleAsset{
+  private String name;
+  private int price;
+  privete String color;
+
+  public TangibleAsset(String neme,int price,String color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+
+  public String getName(){return this.name;}
+  public int getPrice(){return this.name;}
+  public String getColor(){retuen this.color;}
+}
